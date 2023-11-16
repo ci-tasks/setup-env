07845c5cac5049ee0f90e1d1bc579009718bcf47
Svetlin Ralchev <iamralch@users.noreply.github.com>
2023-11-16 16:35:40 +0400

    Do not setup-env if the file does not exist
    Signed-off-by: Svetlin Ralchev <iamralch@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import * as fs from 'fs'
 import * as core from '@actions/core'
 
 async function run(): Promise<void> {
@@ -6,38 +7,42 @@ async function run(): Promise<void> {
     const mask = core.getBooleanInput('mask')
     const expr = core.getBooleanInput('export')
 
-    // eslint-disable-next-line @typescript-eslint/no-require-imports
-    const dotenv = require('dotenv').config({path}) // eslint-disable-line @typescript-eslint/no-var-requires
-    // eslint-disable-next-line @typescript-eslint/no-require-imports
-    const dotexp = require('dotenv-expand').expand(dotenv) // eslint-disable-line @typescript-eslint/no-var-requires
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const variables: any = {}
+    if (fs.existsSync(path)) {
+      // eslint-disable-next-line @typescript-eslint/no-require-imports
+      const dotenv = require('dotenv').config({path}) // eslint-disable-line @typescript-eslint/no-var-requires
+      // eslint-disable-next-line @typescript-eslint/no-require-imports
+      const dotexp = require('dotenv-expand').expand(dotenv) // eslint-disable-line @typescript-eslint/no-var-requires
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const variables: any = {}
 
-    core.info(`Exporting environment variables from ${path}`)
-    core.setOutput('variables', variables)
+      core.info(`Exporting environment variables from ${path}`)
+      core.setOutput('variables', variables)
 
-    for (const name in dotexp.parsed) {
-      const key = name.toLocaleLowerCase()
-      const value = dotexp.parsed[name]
-      // output the value
-      variables[key] = value
-      // export the value
-      if (expr) {
-        core.info(`Exporting environment variables ${name}`)
-        core.exportVariable(name, value)
+      for (const name in dotexp.parsed) {
+        const key = name.toLocaleLowerCase()
+        const value = dotexp.parsed[name]
+        // output the value
+        variables[key] = value
+        // export the value
+        if (expr) {
+          core.info(`Exporting environment variables ${name}`)
+          core.exportVariable(name, value)
+        }
       }
-    }
 
-    for (const name in dotexp.parsed) {
-      const value = dotexp.parsed[name]
-      // mask the value
-      if (mask) {
-        core.info(`Masking environment variables ${name}`)
-        core.setSecret(value)
+      for (const name in dotexp.parsed) {
+        const value = dotexp.parsed[name]
+        // mask the value
+        if (mask) {
+          core.info(`Masking environment variables ${name}`)
+          core.setSecret(value)
+        }
       }
     }
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) {
+      core.setFailed(error.message)
+    }
   }
 }
 
